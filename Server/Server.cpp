10b42f0e3a9e40f1d2bb659ea7211577793bb42b
Mike Sherman <mike.sher08@gmail.com>
2010-11-30 21:27:51 -0500

    Server sends file over wire.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -11,15 +11,13 @@
 
 using namespace std;
 void waitForUpdates();
-void createMonsterArray(Enemy *currentMonsters, string filename);
-void sendLevel(UDPpacket *p, UDPsocket *sd, Enemy* currentMonsters);
+void createMonsterArray(Enemy **currentMonsters, string filename);
+void sendLevel(UDPpacket *p, UDPsocket sd, Enemy **currentMonsters, int arrayLength);
 int lineCount(string fileName);
-Parser *myParser = new Parser();
-Enemy *currentMonsters;
 int main(int argc, char **argv)
 {
 	UDPsocket mySocketDesc;       /* Socket descriptor */
-	UDPpacket *p1, *p2;       /* Pointer to packet memory */
+	UDPpacket *p1, *p2, *p;       /* Pointer to packet memory */
 	IPaddress p1IpAddress, p2IpAddress;
 	int quit;
  
@@ -48,55 +46,48 @@ int main(int argc, char **argv)
 		fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
 		exit(EXIT_FAILURE);
 	}
+	if (!(p = SDLNet_AllocPacket(512)))
+	{
+		fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
+		exit(EXIT_FAILURE);
+	}
  
 	/* Main loop */
-	int numOfLines = 1;
-	numOfLines = lineCount("level1.txt");
-	/* IF WE ARE SEG FAULTING LOOK AT THE LINE BELOW */
-	currentMonsters = new Enemy[numOfLines];
+	int numOfLines = lineCount("level1.txt");
+	Enemy *currentMonsters[numOfLines];
 	if(numOfLines > 0){
 		for(int i = 0; i < numOfLines; i++)
-			currentMonsters[i].update(0,0,0,0,0);
-		createMonsterArray(&currentMonsters[numOfLines], "level1.txt");
+			currentMonsters[i] = new Enemy();
+		/* IF WE ARE SEG FAULTING LOOK AT THE LINE BELOW */
+		createMonsterArray(currentMonsters, "level1.txt");
 	}
 	int count = 0;
 	while (count != 2)
 	{
-	UDPpacket *p;
-		/* Wait a packet. UDP_Recv returns != 0 if a packet is coming */
-		if (SDLNet_UDP_Recv(mySocketDesc, p))
-		{
-			if(count == 0){
-				p1 = p;
-				p = NULL;
-				//send level 1
-				count++;
-			}else if(count == 1 && p1->address.port != p->address.port){
-				p2 = p;
-				//send level 1
+		if(count == 0){
+			if (SDLNet_UDP_Recv(mySocketDesc, p1)){
+				cout << "sending level" << endl;
+				sendLevel(p1, mySocketDesc, currentMonsters, numOfLines);
 				count++;
 			}
-		}
+		}else if (count == 1){
+			if (SDLNet_UDP_Recv(mySocketDesc, p2)){
+				cout << "sending level" << endl;
+				sendLevel(p2, mySocketDesc, currentMonsters, numOfLines);
+				count++;
+			}
+		}else
+			cout << "ok" <<endl;
+		/* Wait a packet. UDP_Recv returns != 0 if a packet is coming */
 	}
-	waitForUpdates();
  
 	/* Clean and exit */
-	SDLNet_FreePacket(p1);
-	SDLNet_FreePacket(p2);
+	SDLNet_FreePacket(p);
 	SDLNet_Quit();
 	return EXIT_SUCCESS;
 }
-void sendLevel(UDPpacket *p, UDPsocket sd, Enemy* currentMonsters){
-	int count = 0;
-	//p->data = (Uint8 *)currentMonsters[count++].toString();
-	SDLNet_UDP_Send(sd, -1, p);
-}
-void waitForUpdates(){
-
-
-
-}
-void createMonsterArray(Enemy *currentMonsters, string filename){
+void createMonsterArray(Enemy **currentMonsters, string filename){
+	Parser *myParser = new Parser();
 	/* read the line. parse the line. create the monster. quit at eof*/
 	ifstream openForParse;
 	int count = 0;
@@ -109,8 +100,35 @@ void createMonsterArray(Enemy *currentMonsters, string filename){
 	//parse the file.
 		constructParams = myParser->CreateMonsterObject(needsParsing);
 	//create the monster
-		currentMonsters[count++].update(constructParams[0], constructParams[1], constructParams[2], constructParams[3], constructParams[4]);
+		currentMonsters[count++]->update(constructParams[0], constructParams[1], constructParams[2], constructParams[3], constructParams[4]);
 	}
+	openForParse.close();
+}
+void sendLevel(UDPpacket *p, UDPsocket sd, Enemy **currentMonsters, int arrayLength){
+	sleep(5);int count = 0;
+	while (count < arrayLength+1){
+		char * temp = currentMonsters[count++]->toString();
+		p->data =(Uint8 *) temp;
+		p->len = strlen(temp)+1;
+		SDLNet_UDP_Send(sd, -1, p);
+		cout << "waiting for reply" << endl;
+		/*while(true){
+			if(SDLNet_UDP_Recv(sd, p)){
+				cout << "GOT IT!" << endl;
+				break;
+			}
+		}*/
+		count++;
+	}
+	p->data = (Uint8 *)"quit";
+	p->len = 5;
+	SDLNet_UDP_Send(sd, -1, p);
+}
+void waitForUpdates(){
+	
+
+	cout << "finished" <<endl;
+
 }
 int lineCount(string fileName){
 	ifstream openForLines;
@@ -120,34 +138,6 @@ int lineCount(string fileName){
 		getline(openForLines, fileName);
 		count++;
 	}
+	openForLines.close();
 	return count;
-}
-		/* comments for later use if needed
-			printf("UDP Packet incoming\n");
-			printf("\tChan:    %d\n", p->channel);
-			printf("\tData:    %s\n", (char *)p->data);
-			printf("\tLen:     %d\n", p->len);
-			printf("\tMaxlen:  %d\n", p->maxlen);
-			printf("\tStatus:  %d\n", p->status);
-			unsigned int add=SDLNet_Read32(&(p->address.host));
-			int port = SDLNet_Read16 (&(p->address.port));
-			printf ("%d\n",add);
-				int a4 = add%256; add/=256;
-				int a3 = add%256; add/=256;
-				int a2 = add%256; add/=256;
-				int a1 = add%256;
-			printf("\tAddress: %d.%d.%d.%d %d\n", a1,a2,a3,a4, port);*/
- 
-			/* Quit if packet contains "quit" *//*
-			if (!strcmp((char *)p->data, "quit"))
-				quit = 1;
-				printf("f");
-				gets((char *)p->data);
-				p->len = 6;
-			SDLNet_UDP_Send(sd, -1, p);
-			if (SDLNet_ResolveHost(&srvadd, "127.0.0.1", 12234))
-			{
-				fprintf(stderr, "SDLNet_ResolveHost(%s %d): %s\n", argv[1], atoi(argv[2], SDLNet_GetError());
-				exit(EXIT_FAILURE);
-			}
-		}	*/	
+}	
