aa884f44a39ed61e76db5ea1ddcb9b5b59d9bce3
Mike Sherman <mike.sher08@gmail.com>
2010-12-02 10:06:04 -0500

    returns an array of enemies

--- Client.cpp
+++ Client.cpp
@@ -38,37 +38,46 @@ Client::~Client(){
 
 Enemy** Client::GetEnemyList(){
 	int quit = 0;
-	Enemy **ListOfEnemies; while (!quit)
-	{
-		strcpy((char *)p->data, "ready");	
-		p->address.host = srvadd.host;	/* Set the destination host */
-		p->address.port = srvadd.port;	/* And destination port */
- 
-		p->len = strlen((char *)p->data) + 1;
-		SDLNet_UDP_Send(sd, -1, p); /* This sets the p->channel */
-		bool firstRead = true;
+	int arraySize = 0;
 		int count = 0;
 		int* params;
+	while (!quit)
+	{
+		strcpy((char *)p->data, "ready");	
+		p->address.host = srvadd.host;
+		p->address.port = srvadd.port;
+ 
+		p->len = strlen((char *)p->data) + 1;
+		SDLNet_UDP_Send(sd, -1, p);
  		while(true){
 			if(SDLNet_UDP_Recv(sd, p)){
-				if(!strcmp((char *)p->data, "quit"))
-					break;
-				if(firstRead){
-					Enemy *Temp[atoi((char *)p->data)];
-					firstRead = false;
-					ListOfEnemies = Temp;
-				}else{
-					//parse the char* //update the monster. move on
-					char * r = (char *)p->data;
-					string b = r;
-					params = myParser->CreateMonsterObject(b);
-					ListOfEnemies[count++] = new Enemy(params[0], params[1], params[2], params[3], params[4]);
-				}
+				arraySize = atoi((char *)p->data);
+				quit = 1;
+				break;
 			}
 		}
-		/* Quit if packet contains "quit" */
-		if (!strcmp((char *)p->data, "quit"))
-			quit = 1;
+		cout <<"I'm out"<<endl;
 	}
-	return ListOfEnemies;
+		cout <<"true!" <<endl;
+	static Enemy *enemyList[100];
+	for (int i = 0; i < 100; i++)
+		enemyList[i] = new Enemy();
+	strcpy((char *)p->data, "ready");	
+	p->len = strlen((char *)p->data) + 1;
+	SDLNet_UDP_Send(sd, -1, p);
+	while(true){
+		if(SDLNet_UDP_Recv(sd, p)){
+			if(!strcmp((char *)p->data, "quit"))
+				break;
+			//parse the char* //update the monster. move on
+			char * r = (char *)p->data;
+			string b = r;
+			params = myParser->CreateMonsterObject(b);
+			enemyList[count++]->update(params[0], params[1], params[2], params[3], params[4]);
+		}
+	}
+	char * asdf = new char[25];
+	enemyList[20]->toString(asdf);
+	printf("asdf, %s\n\n\n", asdf);
+	return enemyList;
 }
