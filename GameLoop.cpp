f98046e202d5bac57684566072638f84a34c5578
Mike Sherman <mike.sher08@gmail.com>
2010-12-03 11:19:43 -0500

    fixed a compile issue

--- GameLoop.cpp
+++ GameLoop.cpp
@@ -86,7 +86,7 @@ void GameLoop::tickActors()
 {
 	player1->update(tick, movePlayer);
 	monster1->update();
-	partEmitter->UpdateShip(tick, player1->getX(), player1->getY());
+	partEmitter->UpdateShip(player1->getX(), player1->getY());
 	for(int i = 0; i < 5; i++)
 		crazies[i]->update();
 }
