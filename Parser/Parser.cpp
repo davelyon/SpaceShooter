6c48a3b0a7ac94947b01873ecba1ce62b7284fca
Mike Sherman <mike.sher08@gmail.com>
2010-12-05 15:19:13 -0500

    refactored monsterparser name, added method declarations

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -8,19 +8,38 @@ Parser::~Parser(){
 int* Parser::CreateMonsterObject(string monsterCreationParams){
 	string uID, x, y, img, points;
 	int* returnable = new int[5];
-	MonsterParser(&monsterCreationParams, &uID);
+	SpaceParser(&monsterCreationParams, &uID);
 	returnable[0] = atoi(uID.c_str());
-	MonsterParser(&monsterCreationParams, &x);
+	SpaceParser(&monsterCreationParams, &x);
 	returnable[1] = atoi(x.c_str());
-	MonsterParser(&monsterCreationParams, &y);
+	SpaceParser(&monsterCreationParams, &y);
 	returnable[2] = atoi(y.c_str());
-	MonsterParser(&monsterCreationParams, &img);
+	SpaceParser(&monsterCreationParams, &img);
 	returnable[3] = atoi(img.c_str());
-	MonsterParser(&monsterCreationParams, &points);
+	SpaceParser(&monsterCreationParams, &points);
 	returnable[4] = atoi(points.c_str());
 	return returnable;
 }
-void Parser::MonsterParser(string *params, string *varToSet){
+float* Parser::Position(string XYPositions){
+	string xPos, yPos, junk;
+	float* returnable = new float[2];
+	SpaceParser(&XYPositions, &junk);
+	SpaceParser(&XYPositions, &xPos);
+	returnable[0] = atof(xPos.c_str());
+	SpaceParser(&XYPositions, &yPos);
+	returnable[1] = atof(yPos.c_str());
+	return returnable;
+}
+float* Parser::OtherPlayer(string XYPositions){
+	string xPos, yPos;
+	float* returnable = new float[2];
+	SpaceParser(&XYPositions, &xPos);
+	returnable[0] = atof(xPos.c_str());
+	SpaceParser(&XYPositions, &yPos);
+	returnable[1] = atof(yPos.c_str());
+	return returnable;
+}
+void Parser::SpaceParser(string *params, string *varToSet){
 	string newParams = *params;
 	string newVarToSet;
 	int found = newParams.find(" ");
