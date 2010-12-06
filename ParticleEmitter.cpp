6e675eccc9dd3eedcfc13d52c76864dbd2b38db8
Dave Lyon <dave@davelyon.net>
2010-12-06 09:40:20 -0500

    Singleton'd particle emitter

--- ParticleEmitter.cpp
+++ ParticleEmitter.cpp
@@ -11,14 +11,29 @@
 
 ParticleEmitter* ParticleEmitter::singletonInstance = NULL;
 
+ParticleEmitter* ParticleEmitter::Instance() {
+	if( singletonInstance == NULL )
+		singletonInstance = new ParticleEmitter();
+	 return singletonInstance;
+}
+
 ParticleEmitter::ParticleEmitter() {
 	rainbow = TRUE;    /* Toggle rainbow effect                              */
 	texture = load_texture(PARTICLE);
-	slowdown = 4.0f; /* Slow Down Particles                                */
+	slowdown = 4.0f; /* Slow Down shipParticles                                */
 	zoom = -8.0f;   /* Used To Zoom Out                                   */
 	col = 0;
+	exploderCounter = 0;
 	
-	/* Reset all the particles */
+	for(loop = 0; loop < 50; loop++) {
+		int color = 1;
+		float xi, yi, zi;
+		xi =  ( float )( ( rand( ) % 50 ) - 26.0f ) * 10.0f;
+		yi = zi = ( float )( ( rand( ) % 50 ) - 25.0f ) * 10.0f;
+		ResetLaserParticle(loop, color, xi, yi, zi, true);
+	}
+	
+	/* Reset all the shipParticles */
 	for ( loop = 0; loop < MAX_PARTICLES; loop++ )
 	{
 		int color = ( loop + 1 ) / ( MAX_PARTICLES / 12 );
@@ -31,14 +46,13 @@ ParticleEmitter::ParticleEmitter() {
 }
 
 void	ParticleEmitter::emitLaser(float xloc, float yloc) {
-
 }
 
 void	ParticleEmitter::emitExploder(float xloc, float yloc) {
 
 }
 
-void	ParticleEmitter::renderParticles(int tick, float ship_x, float ship_y){
+void	ParticleEmitter::renderShipParticles(int tick, float ship_x, float ship_y){
 	glMatrixMode(GL_TEXTURE);
 	glLoadIdentity();
 	glMatrixMode(GL_COLOR);
@@ -48,25 +62,25 @@ void	ParticleEmitter::renderParticles(int tick, float ship_x, float ship_y){
 	glTranslatef(ship_x, ship_y-0.5f, 0.0f);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glScalef(0.3f, 0.3f, 1.0f);
-	/* Modify each of the particles */
+	/* Modify each of the shipParticles */
 	for ( loop = 0; loop < MAX_PARTICLES; loop++ )
 	{
-		if ( particles[loop].active )
+		if ( shipParticles[loop].active )
 		{
 			/* Grab Our Particle X Position */
-			float x = particles[loop].x;
+			float x = shipParticles[loop].x;
 			/* Grab Our Particle Y Position */
-			float y = particles[loop].y;
+			float y = shipParticles[loop].y;
 			/* Particle Z Position + Zoom */
-			float z = particles[loop].z + zoom;
+			float z = shipParticles[loop].z + zoom;
 			
 			/* Draw The Particle Using Our RGB Values,
 			 * Fade The Particle Based On It's Life
 			 */
-			glColor4f( particles[loop].r,
-								particles[loop].g,
-								particles[loop].b,
-								particles[loop].life );
+			glColor4f( shipParticles[loop].r,
+								shipParticles[loop].g,
+								shipParticles[loop].b,
+								shipParticles[loop].life );
 			
 			
 			
@@ -87,27 +101,27 @@ void	ParticleEmitter::renderParticles(int tick, float ship_x, float ship_y){
 			glEnd();
 			
 			/* Move On The X Axis By X Speed */
-			particles[loop].x += particles[loop].xi /
+			shipParticles[loop].x += shipParticles[loop].xi /
 			( slowdown * 1000 );
 			/* Move On The Y Axis By Y Speed */
-			particles[loop].y += particles[loop].yi /
+			shipParticles[loop].y += shipParticles[loop].yi /
 			( slowdown * 1000 );
 			/* Move On The Z Axis By Z Speed */
-			particles[loop].z += particles[loop].zi /
+			shipParticles[loop].z += shipParticles[loop].zi /
 			( slowdown * 1000 );
 			
 			/* Take Pull On X Axis Into Account */
-			particles[loop].xi += particles[loop].xg;
+			shipParticles[loop].xi += shipParticles[loop].xg;
 			/* Take Pull On Y Axis Into Account */
-			particles[loop].yi += particles[loop].yg;
+			shipParticles[loop].yi += shipParticles[loop].yg;
 			/* Take Pull On Z Axis Into Account */
-			particles[loop].zi += particles[loop].zg;
+			shipParticles[loop].zi += shipParticles[loop].zg;
 			
-			/* Reduce Particles Life By 'Fade' */
-			particles[loop].life -= particles[loop].fade;
+			/* Reduce shipParticles Life By 'Fade' */
+			shipParticles[loop].life -= shipParticles[loop].fade;
 			
 			/* If the particle dies, revive it */
-			if ( particles[loop].life < 0.0f )
+			if ( shipParticles[loop].life < 0.0f )
 			{
 				float xi, yi, zi;
 				xi = xspeed +
@@ -121,6 +135,86 @@ void	ParticleEmitter::renderParticles(int tick, float ship_x, float ship_y){
 	}
 }
 
+void	ParticleEmitter::renderLaserParticles(int tick, int laserX, int laserY){
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
+	glMatrixMode(GL_COLOR);
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity( );
+	glTranslatef(laserX, laserY, 0.0f);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glScalef(0.3f, 0.3f, 1.0f);
+
+	for ( loop = 0; loop < 10; loop++ )
+	{
+		if ( laserParticles[loop].active )
+		{
+			/* Grab Our Particle X Position */
+			float x = laserParticles[loop].x;
+			/* Grab Our Particle Y Position */
+			float y = laserParticles[loop].y;
+			/* Particle Z Position + Zoom */
+			float z = laserParticles[loop].z + zoom;
+			
+			/* Draw The Particle Using Our RGB Values,
+			 * Fade The Particle Based On It's Life
+			 */
+			glColor4f( laserParticles[loop].r,
+								laserParticles[loop].g,
+								laserParticles[loop].b,
+								laserParticles[loop].life );
+			
+			/* Build Quad From A Triangle Strip */
+			glBegin( GL_TRIANGLE_STRIP );
+			/* Top Right */
+			glTexCoord2d( 1, 1 );
+			glVertex3f( x + 0.5f, y + 0.5f, z );
+			/* Top Left */
+			glTexCoord2d( 0, 1 );
+			glVertex3f( x - 0.5f, y + 0.5f, z );
+			/* Bottom Right */
+			glTexCoord2d( 1, 0 );
+			glVertex3f( x + 0.5f, y - 0.5f, z );
+			/* Bottom Left */
+			glTexCoord2d( 0, 0 );
+			glVertex3f( x - 0.5f, y - 0.5f, z );
+			glEnd();
+			
+			/* Move On The X Axis By X Speed */
+			laserParticles[loop].x += laserParticles[loop].xi /
+			( slowdown * 1000 );
+			/* Move On The Y Axis By Y Speed */
+			laserParticles[loop].y += laserParticles[loop].yi /
+			( slowdown * 1000 );
+			/* Move On The Z Axis By Z Speed */
+			laserParticles[loop].z += laserParticles[loop].zi /
+			( slowdown * 1000 );
+			
+			
+			/* Reduce laserParticles Life By 'Fade' */
+			laserParticles[loop].life -= laserParticles[loop].fade;
+			
+			/* If the particle dies, revive it */
+			/* If the particle dies, revive it */
+			if ( shipParticles[loop].life < 0.0f )
+			{
+				float xi, yi, zi;
+				xi = xspeed +
+				( float )( ( rand( ) % 60 ) - 32.0f );
+				yi = yspeed +
+				( float)( ( rand( ) % 60 ) - 30.0f );
+				zi = ( float )( ( rand( ) % 60 ) - 30.0f );
+				ResetLaserParticle( loop, col, xi, yi, zi, true );
+			}
+		}
+	}
+	
+	printf("Done with frickin' lasers\n");
+	glFinish();
+
+}
+
 void	ParticleEmitter::renderSetup(void) {
 
 }
@@ -131,35 +225,70 @@ void	ParticleEmitter::renderTeardown(void) {
 void ParticleEmitter::ResetParticle( int num, int color, float xDir, float yDir, float zDir )
 {
 	/* Make the particels active */
-	particles[num].active = TRUE;
-	/* Give the particles life */
-	particles[num].life = 1.0f;
+	shipParticles[num].active = TRUE;
+	/* Give the shipParticles life */
+	shipParticles[num].life = 1.0f;
 	/* Random Fade Speed */
-	particles[num].fade = ( float )( rand( ) %100 ) / 1000.0f + 0.003f;
+	shipParticles[num].fade = ( float )( rand( ) %100 ) / 1000.0f + 0.003f;
 	/* Select Red Rainbow Color */
-	particles[num].r = colors[rand() % 3][0];
+	shipParticles[num].r = colors[rand() % 3][0];
 	/* Select Green Rainbow Color */
-	particles[num].g = colors[rand() % 3][1];
+	shipParticles[num].g = colors[rand() % 3][1];
 	/* Select Blue Rainbow Color */
-	particles[num].b = colors[rand() % 3][2];
+	shipParticles[num].b = colors[rand() % 3][2];
 	/* Set the position on the X axis */
-	particles[num].x = 0.0f;
+	shipParticles[num].x = 0.0f;
 	/* Set the position on the Y axis */
-	particles[num].y = 0.0f;
+	shipParticles[num].y = 0.0f;
 	/* Set the position on the Z axis */
-	particles[num].z = 0.0f;
+	shipParticles[num].z = 0.0f;
 	/* Random Speed On X Axis */
-	particles[num].xi = xDir;
+	shipParticles[num].xi = xDir;
 	/* Random Speed On Y Axi */
-	particles[num].yi = yDir;
+	shipParticles[num].yi = yDir;
 	/* Random Speed On Z Axis */
-	particles[num].zi = zDir;
+	shipParticles[num].zi = zDir;
 	/* Set Horizontal Pull To Zero */
-	particles[num].xg = 0.0f;
+	shipParticles[num].xg = 0.0f;
 	/* Set Vertical Pull Downward */
-	particles[num].yg = -0.8f;
+	shipParticles[num].yg = -0.8f;
 	/* Set Pull On Z Axis To Zero */
-	particles[num].zg = 0.0f;
+	shipParticles[num].zg = 0.0f;
+	
+	return;
+}
+
+void ParticleEmitter::ResetLaserParticle( int num, int color, float xDir, float yDir, float zDir, bool living )
+{
+	laserParticles[num].active = living;
+	/* Give the laserParticles life */
+	laserParticles[num].life = 1.0f;
+	/* Random Fade Speed */
+	laserParticles[num].fade = ( float )( rand( ) %100 ) / 1000.0f + 0.003f;
+	/* Select Red Rainbow Color */
+	laserParticles[num].r = colors[rand() % 3][0];
+	/* Select Green Rainbow Color */
+	laserParticles[num].g = colors[rand() % 3][1];
+	/* Select Blue Rainbow Color */
+	laserParticles[num].b = colors[rand() % 3][2];
+	/* Set the position on the X axis */
+	laserParticles[num].x = 0.0f;
+	/* Set the position on the Y axis */
+	laserParticles[num].y = 0.0f;
+	/* Set the position on the Z axis */
+	laserParticles[num].z = 0.0f;
+	/* Random Speed On X Axis */
+	laserParticles[num].xi = xDir;
+	/* Random Speed On Y Axi */
+	laserParticles[num].yi = yDir;
+	/* Random Speed On Z Axis */
+	laserParticles[num].zi = zDir;
+	/* Set Horizontal Pull To Zero */
+	laserParticles[num].xg = 0.0f;
+	/* Set Vertical Pull Downward */
+	laserParticles[num].yg = -0.8f;
+	/* Set Pull On Z Axis To Zero */
+	laserParticles[num].zg = 0.0f;
 	
 	return;
 }
